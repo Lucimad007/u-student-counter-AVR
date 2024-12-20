e32fe1b54622c9ef79dd0b97a4d271686a65c982
Aminsabouri <matinaminsabouri70@gmail.com>
2024-12-20 14:50:05 +0330

    Test lm35 sensor & Reconfig and reformat Buzzer & design circuit

--- fw/src/keypad.h
+++ fw/src/keypad.h
@@ -5,9 +5,9 @@
 #include "micro_config.h"
 #include "common_macros.h"
 
-#define KEYPAD_PORT_OUT PORTA
-#define KEYPAD_PORT_IN  PINA
-#define KEYPAD_PORT_DIR DDRA
+#define KEYPAD_PORT_OUT PORTB
+#define KEYPAD_PORT_IN  PINB
+#define KEYPAD_PORT_DIR DDRB
 
 void keypad_init(void);
 char scan_keypad(void);
--- fw/src/main.c
+++ fw/src/main.c
@@ -6,13 +6,13 @@
 #include "USART.h"
 
 typedef enum {
-    STATE_MAIN_MENU,
-    STATE_ATTENDANCE_INIT,
-    STATE_STUDENT_MANAGEMENT,
-    STATE_VIEW_PRESENT,
-    STATE_TEMPERATURE_MONITOR,
-    STATE_RETRIEVE_STUDENT_DATA,
-    STATE_TRAFFIC_MONITOR
+	STATE_MAIN_MENU,
+	STATE_ATTENDANCE_INIT,
+	STATE_STUDENT_MANAGEMENT,
+	STATE_VIEW_PRESENT,
+	STATE_TEMPERATURE_MONITOR,
+	STATE_RETRIEVE_STUDENT_DATA,
+	STATE_TRAFFIC_MONITOR
 } State;
 
 State currentState = STATE_MAIN_MENU;
@@ -31,98 +31,98 @@ void handleTrafficMonitor(void);
 //
 //void main(void) {
 //
-    //USART_init(MYUBRR); // Initialize USART with the correct baud rate
+//USART_init(MYUBRR); // Initialize USART with the correct baud rate
 //
-    //// test LM35 for temperature
+//// test LM35 for temperature
 //
-	//ADC_Init();                 /* initialize ADC*/
+//ADC_Init();                 /* initialize ADC*/
 //
-    //DDRB = 0XFF;
-    //PORTB = 0;
-	//
-	//while(1)
-	//{
-	   //PORTB = toCelsius(ADC_Read(7)); // 7 means PA7
-	   //_delay_ms(1000);
-	//}
+//DDRB = 0XFF;
+//PORTB = 0;
 //
-    //
-    //// state machine
-    //
-    //// while (1) {       
-    ////     unsigned char str[] = "hello lucimad!\n";
-    ////     PORTC |= 0b00000001;
-    ////     _delay_ms(500);       
-    ////     //UART_SendString(str); // Send character 'A' every 500 ms
-    ////     UART_SendString(str);
-    ////     PORTC &= 0b11111110;
-    ////     _delay_ms(500);
-    //// }
+//while(1)
+//{
+//PORTB = toCelsius(ADC_Read(7)); // 7 means PA7
+//_delay_ms(1000);
+//}
 //
-    //// int choice;
 //
-    //// while (1) {
-    ////     switch (currentState) {
-    ////         case STATE_MAIN_MENU:
-    ////             displayMainMenu();
-    ////             scanf("%d", &choice);
-    ////             switch (choice) {
-    ////                 case 1:
-    ////                     currentState = STATE_ATTENDANCE_INIT;
-    ////                     break;
-    ////                 case 2:
-    ////                     currentState = STATE_STUDENT_MANAGEMENT;
-    ////                     break;
-    ////                 case 3:
-    ////                     currentState = STATE_VIEW_PRESENT;
-    ////                     break;
-    ////                 case 4:
-    ////                     currentState = STATE_TEMPERATURE_MONITOR;
-    ////                     break;
-    ////                 case 5:
-    ////                     currentState = STATE_RETRIEVE_STUDENT_DATA;
-    ////                     break;
-    ////                 case 6:
-    ////                     currentState = STATE_TRAFFIC_MONITOR;
-    ////                     break;
-    ////                 default:
-    ////                     printf("Invalid choice. Try again.\n");
-    ////             }
-    ////             break;
+//// state machine
 //
-    ////         case STATE_ATTENDANCE_INIT:
-    ////             handleAttendanceInit();
-    ////             currentState = STATE_MAIN_MENU;
-    ////             break;
+//// while (1) {
+////     unsigned char str[] = "hello lucimad!\n";
+////     PORTC |= 0b00000001;
+////     _delay_ms(500);
+////     //UART_SendString(str); // Send character 'A' every 500 ms
+////     UART_SendString(str);
+////     PORTC &= 0b11111110;
+////     _delay_ms(500);
+//// }
 //
-    ////         case STATE_STUDENT_MANAGEMENT:
-    ////             handleStudentManagement();
-    ////             currentState = STATE_MAIN_MENU;
-    ////             break;
+//// int choice;
 //
-    ////         case STATE_VIEW_PRESENT:
-    ////             handleViewPresentStudents();
-    ////             currentState = STATE_MAIN_MENU;
-    ////             break;
+//// while (1) {
+////     switch (currentState) {
+////         case STATE_MAIN_MENU:
+////             displayMainMenu();
+////             scanf("%d", &choice);
+////             switch (choice) {
+////                 case 1:
+////                     currentState = STATE_ATTENDANCE_INIT;
+////                     break;
+////                 case 2:
+////                     currentState = STATE_STUDENT_MANAGEMENT;
+////                     break;
+////                 case 3:
+////                     currentState = STATE_VIEW_PRESENT;
+////                     break;
+////                 case 4:
+////                     currentState = STATE_TEMPERATURE_MONITOR;
+////                     break;
+////                 case 5:
+////                     currentState = STATE_RETRIEVE_STUDENT_DATA;
+////                     break;
+////                 case 6:
+////                     currentState = STATE_TRAFFIC_MONITOR;
+////                     break;
+////                 default:
+////                     printf("Invalid choice. Try again.\n");
+////             }
+////             break;
 //
-    ////         case STATE_TEMPERATURE_MONITOR:
-    ////             handleTemperatureMonitor();
-    ////             currentState = STATE_MAIN_MENU;
-    ////             break;
-    ////         case STATE_RETRIEVE_STUDENT_DATA:
-    ////             handleRetrieveStudentData();
-    ////             currentState = STATE_MAIN_MENU;
-    ////             break;
-    ////         case STATE_TRAFFIC_MONITOR:
-    ////             handleTrafficMonitor();
-    ////             currentState = STATE_MAIN_MENU;
-    ////             break;
+////         case STATE_ATTENDANCE_INIT:
+////             handleAttendanceInit();
+////             currentState = STATE_MAIN_MENU;
+////             break;
 //
-    ////         default:
-    ////             printf("Unknown state. Resetting to Main Menu.\n");
-    ////             currentState = STATE_MAIN_MENU;
-    ////     }
-    //// }
+////         case STATE_STUDENT_MANAGEMENT:
+////             handleStudentManagement();
+////             currentState = STATE_MAIN_MENU;
+////             break;
+//
+////         case STATE_VIEW_PRESENT:
+////             handleViewPresentStudents();
+////             currentState = STATE_MAIN_MENU;
+////             break;
+//
+////         case STATE_TEMPERATURE_MONITOR:
+////             handleTemperatureMonitor();
+////             currentState = STATE_MAIN_MENU;
+////             break;
+////         case STATE_RETRIEVE_STUDENT_DATA:
+////             handleRetrieveStudentData();
+////             currentState = STATE_MAIN_MENU;
+////             break;
+////         case STATE_TRAFFIC_MONITOR:
+////             handleTrafficMonitor();
+////             currentState = STATE_MAIN_MENU;
+////             break;
+//
+////         default:
+////             printf("Unknown state. Resetting to Main Menu.\n");
+////             currentState = STATE_MAIN_MENU;
+////     }
+//// }
 //}
 //
 //
@@ -130,12 +130,25 @@ void handleTrafficMonitor(void);
 int main(void){
 	char Temperature[10];
 	float celsius;
-
-	LCD_Init();                 
-	ADC_Init();                 
-	
+	keypad_init();
+	_delay_ms(100);
+	Buzzer_Init();
+	_delay_ms(100);
+	LCD_Init();
+	_delay_ms(100);
+	ADC_Init();
+	_delay_ms(100);
 	while(1)
 	{
+		 Buzzer_Beep();
+		 _delay_ms(1000);
+
+		 Buzzer_Success();
+		 _delay_ms(1000);
+
+		 Buzzer_CriticalWarning();
+		 _delay_ms(2000);
+		 
 		LCD_String_xy(1,0,"Temperature");
 		celsius = (ADC_Read(0)*4.88);
 		celsius = (celsius/10.00);
@@ -147,64 +160,64 @@ int main(void){
 }
 
 
-void displayMainMenu() 
+void displayMainMenu()
 {
-    printf("1. Start Attendance\n");
-    printf("2. Manage Students\n");
-    printf("3. Monitor Temperature\n");
-    printf("4. Traffic Monitoring\n");
-    printf("Enter your choice: ");
-    // show on LCD
+	printf("1. Start Attendance\n");
+	printf("2. Manage Students\n");
+	printf("3. Monitor Temperature\n");
+	printf("4. Traffic Monitoring\n");
+	printf("Enter your choice: ");
+	// show on LCD
 }
 
-void handleAttendanceInit() 
+void handleAttendanceInit()
 {
-    printf("Attendance Initialized. Waiting for student codes...\n");
-    // logic
-    // submut code or exit
+	printf("Attendance Initialized. Waiting for student codes...\n");
+	// logic
+	// submut code or exit
 }
 
-void handleSubmitCode() 
+void handleSubmitCode()
 {
-    printf("Submitting Student Code...\n");
-    // logic
-    // get error on LCD & buzzer sound if wrong length or format
+	printf("Submitting Student Code...\n");
+	// logic
+	// get error on LCD & buzzer sound if wrong length or format
 }
 
-void handleStudentManagement() 
+void handleStudentManagement()
 {
-    printf("Managing Students...\n");
-    // logic
-    // search for presentation
+	printf("Managing Students...\n");
+	// logic
+	// search for presentation
 }
 
 void handleViewPresentStudents()
 {
-    printf("Viewing Present Students...\n");
-    // logic
-    // display number of present students
-    // then
-    // display names and shift the display
+	printf("Viewing Present Students...\n");
+	// logic
+	// display number of present students
+	// then
+	// display names and shift the display
 }
 
-void handleTemperatureMonitor() 
+void handleTemperatureMonitor()
 {
-    printf("Monitoring Temperature...\n");
-    // logic
-    // ADC and then displaying on LCD
+	printf("Monitoring Temperature...\n");
+	// logic
+	// ADC and then displaying on LCD
 }
 
 void handleRetrieveStudentData()
 {
-    printf("Retrievinng Student Data...\n");
-    // logic
-    // save data to EEPROM using USART
-    // check success and failure and show on LCD
+	printf("Retrievinng Student Data...\n");
+	// logic
+	// save data to EEPROM using USART
+	// check success and failure and show on LCD
 }
 
-void handleTrafficMonitor() 
+void handleTrafficMonitor()
 {
-    printf("Monitoring Traffic...\n");
-    // logic
-    // Display data received from sonar
+	printf("Monitoring Traffic...\n");
+	// logic
+	// Display data received from sonar
 }
