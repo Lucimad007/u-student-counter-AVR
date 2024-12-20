fe83c22eb93fd5eb32ae44b18c2f37b959936c14
Aminsabouri <matinaminsabouri70@gmail.com>
2024-12-20 14:08:33 +0330

    Fix degree_sysmbol issue in sensor & warnings

--- fw/src/main.c
+++ fw/src/main.c
@@ -18,115 +18,133 @@ typedef enum {
 State currentState = STATE_MAIN_MENU;
 
 // state machine
-void displayMainMenu();
-void handleAttendanceInit();
-void handleSubmitCode();
-void handleStudentManagement();
-void handleViewPresentStudents();
-void handleTemperatureMonitor();
-void handleRetrieveStudentData();
-void handleTrafficMonitor();
+void displayMainMenu(void);
+void handleAttendanceInit(void);
+void handleSubmitCode(void);
+void handleStudentManagement(void);
+void handleViewPresentStudents(void);
+void handleTemperatureMonitor(void);
+void handleRetrieveStudentData(void);
+void handleTrafficMonitor(void);
 
 
+//
+//void main(void) {
+//
+    //USART_init(MYUBRR); // Initialize USART with the correct baud rate
+//
+    //// test LM35 for temperature
+//
+	//ADC_Init();                 /* initialize ADC*/
+//
+    //DDRB = 0XFF;
+    //PORTB = 0;
+	//
+	//while(1)
+	//{
+	   //PORTB = toCelsius(ADC_Read(7)); // 7 means PA7
+	   //_delay_ms(1000);
+	//}
+//
+    //
+    //// state machine
+    //
+    //// while (1) {       
+    ////     unsigned char str[] = "hello lucimad!\n";
+    ////     PORTC |= 0b00000001;
+    ////     _delay_ms(500);       
+    ////     //UART_SendString(str); // Send character 'A' every 500 ms
+    ////     UART_SendString(str);
+    ////     PORTC &= 0b11111110;
+    ////     _delay_ms(500);
+    //// }
+//
+    //// int choice;
+//
+    //// while (1) {
+    ////     switch (currentState) {
+    ////         case STATE_MAIN_MENU:
+    ////             displayMainMenu();
+    ////             scanf("%d", &choice);
+    ////             switch (choice) {
+    ////                 case 1:
+    ////                     currentState = STATE_ATTENDANCE_INIT;
+    ////                     break;
+    ////                 case 2:
+    ////                     currentState = STATE_STUDENT_MANAGEMENT;
+    ////                     break;
+    ////                 case 3:
+    ////                     currentState = STATE_VIEW_PRESENT;
+    ////                     break;
+    ////                 case 4:
+    ////                     currentState = STATE_TEMPERATURE_MONITOR;
+    ////                     break;
+    ////                 case 5:
+    ////                     currentState = STATE_RETRIEVE_STUDENT_DATA;
+    ////                     break;
+    ////                 case 6:
+    ////                     currentState = STATE_TRAFFIC_MONITOR;
+    ////                     break;
+    ////                 default:
+    ////                     printf("Invalid choice. Try again.\n");
+    ////             }
+    ////             break;
+//
+    ////         case STATE_ATTENDANCE_INIT:
+    ////             handleAttendanceInit();
+    ////             currentState = STATE_MAIN_MENU;
+    ////             break;
+//
+    ////         case STATE_STUDENT_MANAGEMENT:
+    ////             handleStudentManagement();
+    ////             currentState = STATE_MAIN_MENU;
+    ////             break;
+//
+    ////         case STATE_VIEW_PRESENT:
+    ////             handleViewPresentStudents();
+    ////             currentState = STATE_MAIN_MENU;
+    ////             break;
+//
+    ////         case STATE_TEMPERATURE_MONITOR:
+    ////             handleTemperatureMonitor();
+    ////             currentState = STATE_MAIN_MENU;
+    ////             break;
+    ////         case STATE_RETRIEVE_STUDENT_DATA:
+    ////             handleRetrieveStudentData();
+    ////             currentState = STATE_MAIN_MENU;
+    ////             break;
+    ////         case STATE_TRAFFIC_MONITOR:
+    ////             handleTrafficMonitor();
+    ////             currentState = STATE_MAIN_MENU;
+    ////             break;
+//
+    ////         default:
+    ////             printf("Unknown state. Resetting to Main Menu.\n");
+    ////             currentState = STATE_MAIN_MENU;
+    ////     }
+    //// }
+//}
+//
+//
 
-void main(void) {
+int main(void){
+	char Temperature[10];
+	float celsius;
 
-    USART_init(MYUBRR); // Initialize USART with the correct baud rate
-
-    // test LM35 for temperature
-
-	ADC_Init();                 /* initialize ADC*/
-
-    DDRB = 0XFF;
-    PORTB = 0;
+	LCD_Init();                 
+	ADC_Init();                 
 	
 	while(1)
 	{
-	   PORTB = toCelsius(ADC_Read(7)); // 7 means PA7
-	   _delay_ms(1000);
+		LCD_String_xy(1,0,"Temperature");
+		celsius = (ADC_Read(0)*4.88);
+		celsius = (celsius/10.00);
+		sprintf(Temperature,"%d%cC  ", (int)celsius, degree_sysmbol);/* convert integer value to ASCII string */
+		LCD_String_xy(2,0,Temperature);/* send string data for printing */
+		_delay_ms(1000);
+		memset(Temperature,0,10);
 	}
-
-    
-    // state machine
-    
-    // while (1) {       
-    //     unsigned char str[] = "hello lucimad!\n";
-    //     PORTC |= 0b00000001;
-    //     _delay_ms(500);       
-    //     //UART_SendString(str); // Send character 'A' every 500 ms
-    //     UART_SendString(str);
-    //     PORTC &= 0b11111110;
-    //     _delay_ms(500);
-    // }
-
-    // int choice;
-
-    // while (1) {
-    //     switch (currentState) {
-    //         case STATE_MAIN_MENU:
-    //             displayMainMenu();
-    //             scanf("%d", &choice);
-    //             switch (choice) {
-    //                 case 1:
-    //                     currentState = STATE_ATTENDANCE_INIT;
-    //                     break;
-    //                 case 2:
-    //                     currentState = STATE_STUDENT_MANAGEMENT;
-    //                     break;
-    //                 case 3:
-    //                     currentState = STATE_VIEW_PRESENT;
-    //                     break;
-    //                 case 4:
-    //                     currentState = STATE_TEMPERATURE_MONITOR;
-    //                     break;
-    //                 case 5:
-    //                     currentState = STATE_RETRIEVE_STUDENT_DATA;
-    //                     break;
-    //                 case 6:
-    //                     currentState = STATE_TRAFFIC_MONITOR;
-    //                     break;
-    //                 default:
-    //                     printf("Invalid choice. Try again.\n");
-    //             }
-    //             break;
-
-    //         case STATE_ATTENDANCE_INIT:
-    //             handleAttendanceInit();
-    //             currentState = STATE_MAIN_MENU;
-    //             break;
-
-    //         case STATE_STUDENT_MANAGEMENT:
-    //             handleStudentManagement();
-    //             currentState = STATE_MAIN_MENU;
-    //             break;
-
-    //         case STATE_VIEW_PRESENT:
-    //             handleViewPresentStudents();
-    //             currentState = STATE_MAIN_MENU;
-    //             break;
-
-    //         case STATE_TEMPERATURE_MONITOR:
-    //             handleTemperatureMonitor();
-    //             currentState = STATE_MAIN_MENU;
-    //             break;
-    //         case STATE_RETRIEVE_STUDENT_DATA:
-    //             handleRetrieveStudentData();
-    //             currentState = STATE_MAIN_MENU;
-    //             break;
-    //         case STATE_TRAFFIC_MONITOR:
-    //             handleTrafficMonitor();
-    //             currentState = STATE_MAIN_MENU;
-    //             break;
-
-    //         default:
-    //             printf("Unknown state. Resetting to Main Menu.\n");
-    //             currentState = STATE_MAIN_MENU;
-    //     }
-    // }
 }
-
-
-
 
 
 void displayMainMenu() 
--- fw/src/sensors.h
+++ fw/src/sensors.h
@@ -1,6 +1,8 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
+#define degree_sysmbol 0xdf
+
 void ADC_Init(void);
 int ADC_Read(char channel);
 float toCelsius(int readADC);
